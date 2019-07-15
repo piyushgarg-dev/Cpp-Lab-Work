2fda49765c426af0b0b31d5de10c41575344d44c
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-07-15 12:20:34 +0530

    Added Count

--- student_Management.cpp
+++ student_Management.cpp
@@ -37,14 +37,20 @@ int main(){
     Student s1("Piyush",98,90);
     Student s2("Nakriti",36,89);
     Student s3("Najat",01,90);
+    int count = 0;
     
     if((s1.getname()[0]=='n' || s1.getname()[0]=='N')  && s1.getmarks()>80){
         s1.display();
+        count++;
     }
     if((s2.getname()[0]=='n' || s2.getname()[0]=='N') && s2.getmarks()>80){
         s2.display();
+        count++;
     }
     if((s3.getname()[0]=='n' || s3.getname()[0]=='N') && s3.getmarks()>80){
         s3.display();
+        count++;
     }
+
+    cout << "Total -- " << count << endl;
 }
