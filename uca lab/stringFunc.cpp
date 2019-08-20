b5518ed6480de786ade822d9cf6cf4460224f797
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-08-20 19:54:28 +0530

    Added More String Functions

--- uca lab/stringFunc.cpp
+++ uca lab/stringFunc.cpp
@@ -1,66 +1,133 @@
 /*
-String Functions!
+String Functions
+
+strlen - done
+strcmp - done
+strcmpi - done
+strcat - done
+strrev - done
+strcpy - done
+strupr
+strlwr
+
 */
 #include <iostream>
 using namespace std;
 
-int myStrlen(char str[]){
+int myStrlen(char str[])
+{
     int len = 0;
-    while(str[len]!='\0'){
+    while (str[len] != '\0')
+    {
         len++;
     }
     return len;
 }
-int myStrcmp(char str1[], char str2[]){
+int myStrcmp(char str1[], char str2[])
+{
     int len1 = 0;
     int len2 = 0;
-    while(str1[len1]!='\0'){
+    while (str1[len1] != '\0')
+    {
         len1++;
     }
-    while(str2[len2]!='\0'){
+    while (str2[len2] != '\0')
+    {
         len2++;
     }
-    if(len1!=len2){
+    if (len1 != len2)
+    {
         return 0;
     }
-    else{
-        for(int i=0; i<len1; i++){
-            if(str1[i]!=str2[i]){
-                return int(str1[i]-str2[i]);
+    else
+    {
+        for (int i = 0; i < len1; i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                return int(str1[i] - str2[i]);
             }
         }
         return 1;
     }
 }
 
-int myStrcmpi(char str1[], char str2[]){
+int myStrcmpi(char str1[], char str2[])
+{
     int len1 = 0;
     int len2 = 0;
-    while(str1[len1]!='\0'){
+    while (str1[len1] != '\0')
+    {
         len1++;
     }
-    while(str2[len2]!='\0'){
+    while (str2[len2] != '\0')
+    {
         len2++;
     }
-    if(len1!=len2){
+    if (len1 != len2)
+    {
         return 0;
     }
-    else{
-        for(int i=0; i<len1; i++){
+    else
+    {
+        for (int i = 0; i < len1; i++)
+        {
 
-            if(str1[i]-str2[i]==32 || str1[i]-str2[i] == -32)
-            continue;
+            if (str1[i] - str2[i] == 32 || str1[i] - str2[i] == -32)
+                continue;
 
-            if(str1[i]!=str2[i]){
-                return int(str1[i]-str2[i]);
+            if (str1[i] != str2[i])
+            {
+                return int(str1[i] - str2[i]);
             }
         }
         return 1;
     }
 }
 
+void myStrcat(char *str1, char *str2)
+{
+    int len1, len2 = 0;
 
+    while (*str1 != '\0')
+    {
+        len1++;
+        str1++;
+    }
 
-int main(){
-    cout<<myStrcmp("Piyush","Piyush")<<endl;
+    while (*str2 != '\0')
+    {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+}
+
+void myStrrev(char *str)
+{
+    int len = 0;
+    while (*str != '\0')
+    {
+        len++;
+        str++;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        str--;
+        cout << *str;
+    }
+}
+
+void myStrcpy(char *str, char *value)
+{
+    while (*value != '\0')
+    {
+        *str = *value;
+        str++;
+        value++;
+    }
+}
+
+int main()
+{
 }
