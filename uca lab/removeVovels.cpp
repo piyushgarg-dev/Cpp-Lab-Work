9d4942634831889387f3497a579592c17840bfa1
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-08-26 22:02:56 +0530

    Bug Fixes

--- uca lab/removeVovels.cpp
+++ uca lab/removeVovels.cpp
@@ -1,31 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// void removeVovels(char *str){
-//     int count=0;
-//     int index=1;
-//     while(*str!='\0'){
-//         if(*str=='a'||*str=='e'||*str=='i'||*str=='o'||*str=='u'||*str=='A'||*str=='E'||*str=='I'||*str=='O'||*str=='U'){
-//            *str =' ';
-//             count++;
-//         }
-//         str++;
-
-//     }
-
-// }
-
-void removeVovels(char str[]){
-    int i,j = 0;
-    while(str[i]!='\0'){
-         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
-          cout<<str[i];
-         }
-         i++;
-    }
-    cout<<str<<endl;
-}
-
 
 int main(){
     char str[40];
